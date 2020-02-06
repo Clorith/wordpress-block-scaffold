28b5f518f2d903f113f739ad5c46048640c616f4
Marius L. Jensen <468735+Clorith@users.noreply.github.com>
2020-02-06 17:30:54 +0100

    Create my-block.php

---
+++ src/php/my-block.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: My block
+ * Description: A block!
+ * Version: 1.0.0
+ * Author: Clorith
+ */
+
+namespace clorith\MyBlock;
+
+// Load in the dependencies.
+require_once( __DIR__ . '/includes/class-my-block.php' );
